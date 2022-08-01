a9317add3cabf4b06e7c132c116faee1e4f4f9ce
SAHIL DAFADE <90718067+Sahil089@users.noreply.github.com>
2022-08-01 20:30:38 +0530

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -46,7 +46,7 @@ body.addEventListener("click", e => {
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "08/25/",
+    dayMonth = "08/01",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
